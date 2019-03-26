4cad2bf5d5d54fa9ce380cb40c2603f0e4ff80d0
WKeagle <634938504@qq.com>
2019-03-26 16:38:12 +0800

    first commit

---
+++ src/main/java/com/wukan/demo/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.wukan.demo.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class MainController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "this is the first demo.";
+    }
+}
